0ef3ae22800f91d8453a0b107cab9203142b9e33
Nicolò Matassoli <nico.matassoli@gmail.com>
2021-03-26 23:34:50 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+  el: "#todo__list",
+  data: {
+    lista: ["Latte", "Pasta", "Uova"]
+  },
+  methods: {
+    deleteItem: function() {
+      alert('boh');
+
+    }
+  }
+});
