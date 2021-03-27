19390384bfc3afcebc3eb8ca0b466d88a9ce546b
Nicolò Matassoli <nico.matassoli@gmail.com>
2021-03-27 13:03:38 +0100

    add function

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 var app = new Vue({
   el: "#todo__list",
   data: {
-    lista: ["Latte", "Pasta", "Uova"]
+    lista: ["Latte", "Pasta", "Uova"],
+    listaNew: "",
   },
   methods: {
     deleteItem: function(itemIndex) {
       this.lista.splice(itemIndex, 1);
-    }
+    },
+    addItem: function() {
+      this.lista.push(this.listaNew);
+      this.listaNew = "";
+    },
   }
 });
